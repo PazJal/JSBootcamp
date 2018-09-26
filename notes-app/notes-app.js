398f2a4c5ba86b36311ae1aaa5a8f11561e254ee
Paz Jaldety <paz@coperato.local>
2018-09-26 15:58:38 +0300

    Finished lecture 62

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -31,6 +31,20 @@ const filters = {
   searchText: ''
 }
 
+// const user = {
+//   name: 'Andrew',
+//   age: 27
+// }
+// const userJSON = JSON.stringify(user);
+// console.log(userJSON);
+
+// localStorage.setItem('user' , userJSON );
+
+const userJSON = localStorage.getItem('user');
+const user = JSON.parse(userJSON);
+console.log(`${user.name} and ${user.age}`);
+
+
 const renderNotes = function (notes , filters) {
   const filteredNotes = notes.filter((note) => {
     
